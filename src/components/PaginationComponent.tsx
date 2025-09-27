6954de6f3cb7a53dc617af248cbccb5bc32292e8
Pierre Carion <pcarion@gmail.com>
2025-09-27 06:54:20 -0700

    add blog pages

---
+++ src/components/PaginationComponent.tsx
@@ -0,0 +1,96 @@
+import * as React from "react"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+
+interface PaginationComponentProps {
+  currentPage: number
+  totalPages: number
+  baseUrl: string
+}
+
+export function PaginationComponent({
+  currentPage,
+  totalPages,
+  baseUrl,
+}: PaginationComponentProps) {
+  const generatePageNumbers = () => {
+    const pages: (number | string)[] = []
+    const maxVisible = 5
+
+    if (totalPages <= maxVisible) {
+      for (let i = 1; i <= totalPages; i++) {
+        pages.push(i)
+      }
+    } else {
+      if (currentPage <= 3) {
+        for (let i = 1; i <= 4; i++) {
+          pages.push(i)
+        }
+        pages.push("ellipsis")
+        pages.push(totalPages)
+      } else if (currentPage >= totalPages - 2) {
+        pages.push(1)
+        pages.push("ellipsis")
+        for (let i = totalPages - 3; i <= totalPages; i++) {
+          pages.push(i)
+        }
+      } else {
+        pages.push(1)
+        pages.push("ellipsis")
+        for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+          pages.push(i)
+        }
+        pages.push("ellipsis")
+        pages.push(totalPages)
+      }
+    }
+
+    return pages
+  }
+
+  const pageNumbers = generatePageNumbers()
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        {currentPage > 1 && (
+          <PaginationItem>
+            <PaginationPrevious
+              href={`${baseUrl}${currentPage - 1}`}
+            />
+          </PaginationItem>
+        )}
+
+        {pageNumbers.map((page, index) => (
+          <PaginationItem key={index}>
+            {page === "ellipsis" ? (
+              <PaginationEllipsis />
+            ) : (
+              <PaginationLink
+                href={`${baseUrl}${page}`}
+                isActive={currentPage === page}
+              >
+                {page}
+              </PaginationLink>
+            )}
+          </PaginationItem>
+        ))}
+
+        {currentPage < totalPages && (
+          <PaginationItem>
+            <PaginationNext
+              href={`${baseUrl}${currentPage + 1}`}
+            />
+          </PaginationItem>
+        )}
+      </PaginationContent>
+    </Pagination>
+  )
+}
