70972f167d659e8c1bb002afeca57e5fe82b055b
Pierre Carion <pcarion@gmail.com>
2025-09-27 07:48:25 -0700

    Styling (#1)

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,10 +8,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://pcarion.com',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap(), react(), icon()],
 
   vite: {
     plugins: [tailwindcss()],
--- src/content.config.ts
+++ src/content.config.ts
@@ -13,7 +13,23 @@ const blog = defineCollection({
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
 			heroImage: image().optional(),
+			tags: z.array(z.string()).optional(),
+			draft: z.boolean().optional(),
 		}),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+	loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/projects' }),
+	schema: ({ image }) =>
+	  z.object({
+		name: z.string(),
+		description: z.string(),
+		tags: z.array(z.string()),
+		image: image(),
+		link: z.string().url(),
+		startDate: z.coerce.date().optional(),
+		endDate: z.coerce.date().optional(),
+	  }),
+  })
+
+export const collections = { blog, projects };
